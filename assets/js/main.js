ffd0d599310c8dafa1df2b0fa92e8993348dc556
DamianoCasolari <damianocasolari@gmail.com>
2023-03-10 16:44:36 +0100

    add dynamic grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+const rowEl = document.querySelector(".row")
 
 for (let i = 0; i < 100; i++) {
 let fizz_number = i + 1;
@@ -10,4 +10,9 @@ if (fizz_number % 15 == 0) {
     fizz_number = "Fizz"; 
  }
  console.log(fizz_number)
+rowEl.innerHTML += `<div class="box_fizz col-2 p-1">
+<div class="card d-flex align-items-center justify-content-center fs-4">
+    ${fizz_number}
+</div>
+</div>`
 }
