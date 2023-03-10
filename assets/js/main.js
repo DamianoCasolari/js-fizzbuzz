1b76dbd7e2fd4606414a26af081fa5efb674a9be
DamianoCasolari <damianocasolari@gmail.com>
2023-03-10 19:20:01 +0100

    change color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,41 @@
 const rowEl = document.querySelector(".row")
-
-for (let i = 0; i < 100; i++) {
-    let fizz_number = i + 1;
+let colorCLass;
+for (let i = 1; i < 101; i++) {
+    let fizz_number = i;
     if (fizz_number % 15 == 0) {
         fizz_number = "FizzBuzz";
+        colorCLass = "my_baground_fizzBuzz"
     } else if (fizz_number % 5 == 0) {
         fizz_number = "Buzz";
+        colorCLass = "my_baground_buzz"
     } else if (fizz_number % 3 == 0) {
         fizz_number = "Fizz";
+        colorCLass = "my_baground_fizz"
+    } else {
+        colorCLass = "my_baground_standard"
     }
     console.log(fizz_number)
     rowEl.innerHTML += `<div class="box_fizz col-6 col-sm-3 col-md-2 p-2">
-<div class="card d-flex align-items-center justify-content-center fs-3 fw-bold">
+    <div class="card d-flex align-items-center justify-content-center fs-3 fw-bold px-1 ${colorCLass} ">
     ${fizz_number}
-</div>
-</div>`
+       </div>
+    </div>`
+
+    // This doesn't work and i don't know why 
+
+    // let myBox = document.querySelector(".box_fizz > .card")
 
 
-}
+    // if (fizz_number % 15 == 0) {
+    //     myBox.classList.add("my_baground_fizzBuzz")
+    // } else if (fizz_number % 3 == 0) {
+    //     myBox.classList.add("my_baground_fizz");
+    // } else if (fizz_number % 5 == 0) {
+    //     myBox.classList.add("my_baground_buzz");
+    // } else {
+    //     myBox.classList.add("my_baground_standard")
+    }
+
+
+
+
